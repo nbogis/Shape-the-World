2dcc2cf7be5120bac3dc3ddf53ed4172f1efcde3
Nidaa <bent_al7ejaz90@hotmail.com>
2015-04-19 18:21:22 -0700

    Started LCD interface

---
+++ Lab15_SpaceInvaders/spaceinvaders.d
@@ -0,0 +1,5 @@
+.\spaceinvaders.o: SpaceInvaders.c
+.\spaceinvaders.o: ..//tm4c123gh6pm.h
+.\spaceinvaders.o: Nokia5110.h
+.\spaceinvaders.o: Random.h
+.\spaceinvaders.o: TExaS.h
---
+++ Lab15_SpaceInvaders/texas.d
@@ -0,0 +1,2 @@
+.\texas.o: TExaS.c
+.\texas.o: TExaS.h
